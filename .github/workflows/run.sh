edf7531ed909336e71d055c04a91daebdb02a09f
Vlasenko Viktoriya <vvlasenko@lanit.ru>
2024-07-05 23:26:41 +0300

    add-docker sprint 16 version 11

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,5 +1,5 @@
 docker-compose up --detach
 chmod a+x ./tests/.github/workflows/wait-for-it.sh
-./tests/.github/workflows/wait-for-it.sh -t 120 localhost:8080
-./tests/.github/workflows/wait-for-it.sh -t 120 localhost:9090
+./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080
+./tests/.github/workflows/wait-for-it.sh -t 60 localhost:9090
 docker-compose logs
